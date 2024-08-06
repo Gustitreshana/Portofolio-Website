d089330ad23be493dffff4ddb9fa4a6ee37cad42
Gustitreshana <gustith2105@gmail.com>
2024-08-07 02:44:02 +0700

    add link for scroll by id

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center p-4 px-20 border-b border-emerald-600">
+    <div className="fixed top-0 left-0 right-0 flex justify-between items-center p-4 px-20 z-1">
         <div>
             <h1 className="text-2xl font-bold text-emerald-600">
                 Gusti
@@ -9,11 +9,11 @@ const Navbar = () => {
 
         <div>
             <ul className="flex items-center gap-8">
-                <li className="text-xl font-bold hover:text-emerald-600 cursor-pointer transition-all duration-300">Home</li>
-                <li className="text-xl font-bold hover:text-emerald-600 cursor-pointer transition-all duration-300">About</li>
-                <li className="text-xl font-bold hover:text-emerald-600 cursor-pointer transition-all duration-300">Education</li>
-                <li className="text-xl font-bold hover:text-emerald-600 cursor-pointer transition-all duration-300">Skills</li>
-                <li className="text-xl font-bold hover:text-emerald-600 cursor-pointer transition-all duration-300">Projects</li>
+                <li className="text-xl font-bold hover:text-emerald-600 cursor-pointer transition-all duration-300"><a href="#home">Home</a></li>
+                <li className="text-xl font-bold hover:text-emerald-600 cursor-pointer transition-all duration-300"><a href="#about">About</a></li>
+                <li className="text-xl font-bold hover:text-emerald-600 cursor-pointer transition-all duration-300"><a href="#education">Education</a></li>
+                <li className="text-xl font-bold hover:text-emerald-600 cursor-pointer transition-all duration-300"><a href="#skills">Skills</a></li>
+                <li className="text-xl font-bold hover:text-emerald-600 cursor-pointer transition-all duration-300"><a href="#projects">Projects</a></li>
             </ul>
         </div>
     </div>
