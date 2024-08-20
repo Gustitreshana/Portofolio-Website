e531deb9b72d5549bc0d3b816d6d689963aa5c78
Gustitreshana <gustith2105@gmail.com>
2024-08-21 00:34:14 +0700

    feat: add handleOpenMasakin and handleOpenZeroHunger

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,6 +1,16 @@
+"use client";
 import Image from "next/image";
 
 const Projects = () => {
+
+  const handleOpenMasakin = () => {
+    window.open("https://masakin-fe.vercel.app/");
+  };
+
+  const handleOpenZeroHunger = () => {
+    window.open("https://final-projec-git-605a51-christopher-jordan-ian-garcias-projects.vercel.app/");
+  };
+
   return (
     <div id="projects" className="flex flex-col justify-center items-center h-screen">
       <div>
@@ -10,7 +20,7 @@ const Projects = () => {
       </div>
 
       <div className="flex justify-center items-center gap-10">
-        <div className="flex justify-center items-center gap-10 mt-20">
+        <div className="flex justify-center items-center gap-10 mt-20 cursor-pointer transition-all duration-300 hover:scale-105" onClick={handleOpenMasakin}>
           <Image
             src="/Masakin-landing-page.png"
             alt="Masakin Landing Page"
@@ -31,7 +41,7 @@ const Projects = () => {
           />
         </div>
 
-        <div className="mt-20">
+        <div className="mt-20 cursor-pointer transition-all duration-300 hover:scale-105" onClick={handleOpenZeroHunger}>
           <Image
             src="/ZeroHunger-landing-page.png"
             alt="ZeroHunger Landing Page"
